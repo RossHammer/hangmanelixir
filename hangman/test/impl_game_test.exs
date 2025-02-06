f4fc7dee12bd6164c35952c5fc06ab4f0f9fb7cc
Ross Hammermeister <3009978+RossHammer@users.noreply.github.com>
2025-02-06 11:24:46 -0700

    Adding tests

---
+++ hangman/test/impl_game_test.exs
@@ -0,0 +1,21 @@
+defmodule HangmanImplGameTest do
+  use ExUnit.Case
+  alias Hangman.Impl.Game
+
+  test "new game returns structure" do
+    game = Game.new_game()
+
+    assert game.turns_left == 7
+    assert game.game_state == :initializing
+    assert length(game.letters) > 0
+    assert Enum.all?(game.letters, fn letter -> letter == String.downcase(letter) end)
+  end
+
+  test "new game returns correct word" do
+    game = Game.new_game("wombat")
+
+    assert game.turns_left == 7
+    assert game.game_state == :initializing
+    assert game.letters == ["w", "o", "m", "b", "a", "t"]
+  end
+end
