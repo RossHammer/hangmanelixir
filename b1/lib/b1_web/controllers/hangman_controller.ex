66541a91ef8ad46060148f53cdf01aed944c370a
Ross Hammermeister <3009978+RossHammer@users.noreply.github.com>
2025-02-10 15:26:47 -0700

    Initial hangman game

--- b1/lib/b1_web/controllers/hangman_controller.ex
+++ b1/lib/b1_web/controllers/hangman_controller.ex
@@ -4,4 +4,27 @@ defmodule B1Web.HangmanController do
   def index(conn, _params) do
     render(conn, :index)
   end
+
+  def new(conn, _params) do
+    game = Hangman.new_game()
+    tally = Hangman.tally(game)
+
+    conn
+    |> put_session(:game, game)
+    |> render(:game, tally: tally)
+  end
+
+  def update(conn, params) do
+    guess = params["make_move"]["guess"]
+
+    tally =
+      conn
+      |> get_session(:game)
+      |> Hangman.make_move(guess)
+
+    conn
+
+    put_in(conn.params["make_move"]["guess"], "")
+    |> render(:game, tally: tally)
+  end
 end
