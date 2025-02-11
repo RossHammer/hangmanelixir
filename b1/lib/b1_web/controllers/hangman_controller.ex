788fd74ff0d8a14b73c90f45a5cfe0c9512b8e70
Ross Hammermeister <3009978+RossHammer@users.noreply.github.com>
2025-02-10 19:04:12 -0700

    ui improvements

--- b1/lib/b1_web/controllers/hangman_controller.ex
+++ b1/lib/b1_web/controllers/hangman_controller.ex
@@ -7,24 +7,29 @@ defmodule B1Web.HangmanController do
 
   def new(conn, _params) do
     game = Hangman.new_game()
-    tally = Hangman.tally(game)
 
     conn
     |> put_session(:game, game)
-    |> render(:game, tally: tally)
+    |> redirect(to: ~p"/hangman/current")
   end
 
   def update(conn, params) do
     guess = params["make_move"]["guess"]
 
+    put_in(conn.params["make_move"]["guess"], "")
+    |> get_session(:game)
+    |> Hangman.make_move(guess)
+
+    redirect(conn, to: ~p"/hangman/current")
+  end
+
+  @spec show(Plug.Conn.t(), any()) :: Plug.Conn.t()
+  def show(conn, _params) do
     tally =
       conn
       |> get_session(:game)
-      |> Hangman.make_move(guess)
+      |> Hangman.tally()
 
-    conn
-
-    put_in(conn.params["make_move"]["guess"], "")
-    |> render(:game, tally: tally)
+    render(conn, :game, tally: tally)
   end
 end
