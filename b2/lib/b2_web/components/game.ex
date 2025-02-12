9efa3fcf59ec7097a97309fcdd720bbbd2471c06
Ross Hammermeister <3009978+RossHammer@users.noreply.github.com>
2025-02-12 13:40:00 -0700

    Adding word view

--- b2/lib/b2_web/components/game.ex
+++ b2/lib/b2_web/components/game.ex
@@ -7,9 +7,14 @@ defmodule B2Web.Components.Game do
     {:ok, assign(socket, game: game, tally: tally)}
   end
 
+  def handle_event("make_move", %{"key" => key}, socket) do
+    tally = Hangman.make_move(socket.assigns.game, key)
+    {:noreply, assign(socket, :tally, tally)}
+  end
+
   def render(assigns) do
     ~H"""
-    <div>
+    <div phx-window-keyup="make_move">
       <.live_component module={__MODULE__.Figure} tally={@tally} id="Figure" />
       <.live_component module={__MODULE__.Alphabet} tally={@tally} id="Alphabet" />
       <.live_component module={__MODULE__.WordSoFar} tally={@tally} id="WordSoFar" />
--- b2/lib/b2_web/components/game/word_so_far.ex
+++ b2/lib/b2_web/components/game/word_so_far.ex
@@ -5,10 +5,35 @@ defmodule B2Web.Components.Game.WordSoFar do
     {:ok, socket}
   end
 
+  @states %{
+    already_used: "You've already used that letter",
+    bad_guess: "That's not in the word",
+    good_guess: "Good guess!",
+    initializing: "Type or click on your first guess",
+    lost: "Sorry, you lost...",
+    won: "You won!"
+  }
+
+  defp state_name(state) do
+    @states[state] || "Unknown state"
+  end
+
   def render(assigns) do
     ~H"""
     <div>
-      In WordSoFar
+      {@tally.game_state |> state_name}
+      <span class="flex gap-3 text-4xl">
+        <div
+          :for={ch <- @tally.letters}
+          class={
+            if ch == "_",
+              do: "text-gray-500",
+              else: "text-green-400"
+          }
+        >
+          {ch}
+        </div>
+      </span>
     </div>
     """
   end
