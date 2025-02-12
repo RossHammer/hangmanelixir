e90207e344776895686cf3654ddf81bdf05cc7ca
Ross Hammermeister <3009978+RossHammer@users.noreply.github.com>
2025-02-12 13:59:11 -0700

    Adding letter suggestions

--- b2/lib/b2_web/components/game/alphabet.ex
+++ b2/lib/b2_web/components/game/alphabet.ex
@@ -2,13 +2,27 @@ defmodule B2Web.Components.Game.Alphabet do
   use B2Web, :live_component
 
   def mount(socket) do
-    {:ok, socket}
+    letters = ?a..?z |> Enum.map(&<<&1::utf8>>)
+    {:ok, assign(socket, :letters, letters)}
   end
 
   def render(assigns) do
     ~H"""
-    <div>
-      In Alphabet
+    <div class="flex flex-wrap gap-2 justify-center text-2xl">
+      <div
+        :for={letter <- @letters}
+        phx-click="make_move"
+        phx-value-key={letter}
+        class={
+          cond do
+            Enum.member?(@tally.letters, letter) -> "text-green-400"
+            Enum.member?(@tally.used, letter) -> "text-red-300"
+            true -> "text-blue-300"
+          end
+        }
+      >
+        {letter}
+      </div>
     </div>
     """
   end
